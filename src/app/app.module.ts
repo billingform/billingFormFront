2f44ca766275ae5ca5b1c7772666298e87d5c0d9
Giant <giant@Giantde-MacBook-Pro.local>
2019-12-29 15:02:28 +0800

    create new project and import bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NgxParisModule, HttpDefaultOptions, JWTOptions } from 'ngx-paris';
+import { environment } from 'src/environments/environment';
+
+export class MonitorHttpDefaultOptions extends HttpDefaultOptions {
+  baseApiURL = environment.baseApiUrl;// input api base url.
+}
+
+export class MonitorJWTOptions extends JWTOptions {
+  key = 'user';
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +22,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxParisModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [{
+    provide: HttpDefaultOptions,
+    useClass: MonitorHttpDefaultOptions
+  },
+  {
+    provide: JWTOptions,
+    useClass: MonitorJWTOptions
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
