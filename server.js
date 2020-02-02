49aab8ce553063a5355b2d3573470947408cacca
Giant <giant@Giantde-MacBook-Pro.local>
2020-02-02 13:57:44 +0800

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
- 
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/form-service'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/form-service/index.html'));
 });
- 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
