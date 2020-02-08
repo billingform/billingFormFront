d9483f3cc2973be8e6b4353f3385b50eb4818a5b
Giant <giant@Giantde-MacBook-Pro.local>
2020-02-08 21:52:17 +0800

    build

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/formService'));
+app.use(express.static(__dirname + '/formService'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/formService/index.html'));
+  res.sendFile(path.join(__dirname + 'formService', 'index.html'));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
