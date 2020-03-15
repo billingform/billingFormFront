e295a9798934c5fe7377175248b05b3d81309f57
Giant <giant@Giantde-MacBook-Pro.local>
2020-03-15 16:56:54 +0800

    tab pages

--- src/app/pages/setting/setting.component.ts
+++ src/app/pages/setting/setting.component.ts
@@ -13,6 +13,7 @@ export class SettingComponent implements OnInit {
   brands = [];
   colors = [];
   isEdit = false;
+  tabPageNum = 0;
 
   constructor() {
   }
@@ -23,6 +24,16 @@ export class SettingComponent implements OnInit {
       { "brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345" },
       { "brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr" },
       { "brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh" },
+      { "brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34" },
+      { "brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj" },
+      { "brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr" },
+      { "brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34" },
+      { "brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5" },
+      { "brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s" },
+      { "brand": "VW", "year": 123, "color": "Orange", "vin": "dsad231ff" },
+      { "brand": "Audi", "year": 123, "color": "Black", "vin": "gwregre345" },
+      { "brand": "Renault", "year": 123, "color": "Gray", "vin": "h354htr" },
+      { "brand": "BMW", "year": 123, "color": "Blue", "vin": "j6w54qgh" },
       { "brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34" },
       { "brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj" },
       { "brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr" },
@@ -60,6 +71,10 @@ export class SettingComponent implements OnInit {
       { label: 'Orange', value: 'Orange' },
       { label: 'Blue', value: 'Blue' }
     ];
+  }
+
+  handleChange($event){
+    this.tabPageNum = $event.index;
   }
 }
 
